a135d31f6bf01d3a97baaf5dab084dc332dd3235
gongzhenzhen <2206463420@qq.com>
2021-12-16 17:46:30 +0800

    线程的中断，阻塞异常中断、逻辑中断标志中断

--- src/thread/Callable.java
+++ src/thread/Callable.java
@@ -21,7 +21,7 @@ public class Callable {
         executorService.submit(futureTask);
         System.out.println("牛");
         Thread.sleep(50);
-        //取消任务
+        //取消任务（只是调用线程的中断方法，如果线程没有阻塞的方法来抛出中断异常，那只能逻辑判断中断标志来中断线程）
         System.out.println(futureTask.cancel(true));
         //判断任务是不是被取消了
         System.out.println(futureTask.isCancelled());
